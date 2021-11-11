42fd252e8d95eb9c23b071dcdc13003f56b214d2
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-11-11 12:31:26 +0500

    update

---
+++ resources/views/frontend/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.frontend.master')
+
+
+@section('title')
+Home
+@endsection
+
+@section('content')
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/',[FrontendController::class,'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
